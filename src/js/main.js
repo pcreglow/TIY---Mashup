5059165c6d70a9cfdedc60144fd1678cfc32fb7e
Phillip Creglow <phillipcreglow@msn.com>
2015-06-12 15:44:57 -0400

    Clicky tabs yo!

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,14 @@
-(function(window){
+(function(window) {
 
-$('button').click(function(){
-  console.log('Click here!');
-//What happens when you click?
-//Add class 'active content', remove class "active content"
+  $('#weather').click(function() {
+    $('.tab-1 div').toggleClass('inactive-content');
+    $('.tab-1 div').toggleClass('.active-content');
+  });
 
-
-$('.tab-1 div').toggleClass('inactive-content');
-$('.tab-1 div').toggleClass('.active-content');
-
-$('.tab-2 div').toggleClass('inactive-content');
-$('.tab-2 div').toggleClass('.active-content');
-
-
-
-
-
-})
-
+  $('#nytimes').click(function() {
+    $('.tab-2 div').toggleClass('inactive-content');
+    $('.tab-2 div').toggleClass('.active-content');
+  });
 
 
 })(window);
