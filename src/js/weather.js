8f606f07d60b02a5ae56549ce95f6d5a0812eba0
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-13 12:49:29 -0400

    Working weather data. Will come back to converting temp later.

--- src/js/weather.js
+++ src/js/weather.js
@@ -5,9 +5,14 @@
 
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=10007,us").then(function(weather) {
 
-  console.log(weather);
   $name = $('.cityName');
   $name.text(weather.name);
+
+  $cityTemp = $('.cityTemp');
+  $cityTemp.text(weather.main.temp);
+
+  $cityDescription = $('.cityDescription');
+  $cityDescription.text(weather.weather[0].description);
 
 });
 
