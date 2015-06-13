545dd728422246e0dd6cfafea6506cfa94f53ced
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-13 17:39:05 -0400

    Removed non working code from flickr.js

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -1,11 +1,6 @@
-// $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang=en-us&format=json&jsoncallback=?").then(function(photos) {
-//
-//
-//   $title = ('.title');
-//   $title.text(photos.items[0].title);
-//
-//
-//   // $cityTemp = $('.cityTemp');
-//   // $cityTemp.text(weather.main.temp);
-//
-// });
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang=en-us&format=json&jsoncallback=?").then(function(photos) {
+
+  console.log(photos);
+
+
+});
