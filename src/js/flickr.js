4179a3ad5d0cdfd8cf712966310781cd96aed709
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-13 17:52:50 -0400

    A step in the right direction for flickr

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -2,5 +2,10 @@ $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang
 
   console.log(photos);
 
+  $media = $('.media');
+  $media.text(photos.items[0].title);
+
+  // $name = $('.cityName');
+  // $name.text(weather.name);
 
 });
