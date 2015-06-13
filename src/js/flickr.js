8735c8be1cfdab4c2a66ac665860d85bc78c3788
Phillip Creglow <phillipcreglow@msn.com>
2015-06-13 18:54:17 -0400

    Added flickr API, pulling 6 photos into unit-span-1

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -2,8 +2,23 @@ $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang
 
   console.log(photos);
 
-  $media = $('img#media');
+  $media = $('img#media-1');
   $media.attr('src', photos.items[0].media.m);
+
+  $media = $('img#media-2');
+  $media.attr('src', photos.items[1].media.m);
+
+  $media = $('img#media-3');
+  $media.attr('src', photos.items[2].media.m);
+
+  $media = $('img#media-4');
+  $media.attr('src', photos.items[3].media.m);
+
+  $media = $('img#media-5');
+  $media.attr('src', photos.items[4].media.m);
+
+  $media = $('img#media-6');
+  $media.attr('src', photos.items[5].media.m);
 
   // $name = $('.cityName');
   // $name.text(weather.name);
