56efda00a89b9c5d9fa547008e43141610745ba5
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-13 03:22:22 -0400

    Can't get Flickr API to work yet. Pivoting to weather for now.

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -1,22 +1,41 @@
-function(window) {
-
-  $(document).ready(function() {
-
-
-    $.getJSON("https: //api.flickr.com/services/feeds/photos_public.gne", format = json, tags: "New York").then(function() {
-
-      // $('body')
-
-
-    });
-
-
-
-
-
-
-
-
-  }); //End of document ready function
-
-})(window);
+  // (function(window) {
+  //
+  //   $.getJSON("https: //api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+  //     tags: "New York"
+  //   }, function(data) {
+  //
+  //     $.each(data.items, function(i, item) {
+  //
+  //       $('body').attr("src", item.media.m).appendTo('$body');
+  //       if (i == 3) return false;
+  //
+  //     });
+  //
+  //   });
+  //
+  //
+  //
+  //   var Flickr = require("flickrapi"),
+  //     flickrOptions = {
+  //       api_key: "2ff81482e3b813d641b9c306b23914f1",
+  //       secret: "ee85a5b315c6330f"
+  //     };
+  //   // .then(function(data) {
+  //
+  //   //
+  //   //
+  //   //     Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+  //   //       // we can now use "flickr" as our API object,
+  //   //       // but we can only call public methods and access public data
+  //   //     });
+  //   //
+  //   //
+  //   //
+  //   //   });
+  //   //
+  //   // function jsonFlickrFeed(data) {
+  //   //   console.log(data);
+  //   // };
+  //
+  //
+  // })(window);
