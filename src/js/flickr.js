d4dd7a19ac40929b916d589d443f480860035d9b
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-13 14:18:15 -0400

    Last try for a failing Flickr feed. Coming back to this later

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -1,16 +1,11 @@
-(function(window) {
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang=en-us&format=json&jsoncallback=?").then(function(photos) {
 
 
-  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?&tags=nyc&lang=en-us&format=json&jsoncallback=?").then(function(photos) {
+  $title = ('.title');
+  $title.text(photos.items[0].title);
 
 
-    $title = ('.title');
-    $title.text(photos.items[0].title);
+  // $cityTemp = $('.cityTemp');
+  // $cityTemp.text(weather.main.temp);
 
-
-    // $cityTemp = $('.cityTemp');
-    // $cityTemp.text(weather.main.temp);
-
-  });
-
-})(window);
+});
